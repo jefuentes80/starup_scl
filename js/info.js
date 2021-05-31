50d167e55c6a2880d8051334a56222c16e7d92ac
Fercholuch <69003173+Fercholuch@users.noreply.github.com>
2021-05-31 13:38:32 -0600

    Add files via upload

---
+++ js/info.js
@@ -0,0 +1,27 @@
+var titulo = document.getElementById('saludo');
+
+function cambiarcolor (){
+    
+    titulo.classList.remove("saludo");
+    titulo.classList.add("nuevaClase")
+} 
+
+var caja_html= document.getElementById('caja');
+
+
+function funcionCaja (){
+    
+    caja_html.classList.remove("estilo_1");
+    caja_html.classList.add("estilo_2");
+}; 
+
+
+/*-------------------------------------------*/
+
+var btn_html = document.getElementById('boton');
+
+btn_html.addEventListener('click', cambiarcolor);
+btn_html.addEventListener('click', funcionCaja);
+
+/*----------------------------------------------------------*/
+
